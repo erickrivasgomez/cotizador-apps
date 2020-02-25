1432d512bd2ae78b37319fb8d551f9eafbe6d891
marioCarbajalScio <60710502+marioCarbajalScio@users.noreply.github.com>
2020-02-25 16:00:54 -0600

    Email a Correo de scio

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
-import sendEmail from './services/email';
 import FormEmail from './components/form-email'
-
-/*
-class Price extends Component{
-  state = {
-    name:String,
-    total:Number,
-  }
-
-  render(){
-    return (
-      
-    )
-  }
-}*/
 
 function App() {
 
--- src/services/email.js
+++ src/services/email.js
@@ -1,22 +1,44 @@
-import React from 'react';
+//import React from 'react';
 import emailjs from 'emailjs-com';
-import templateEmail from '../components/template'
+//import templateEmail from '../components/template'
+
+const service_id = "default_service";//Gmail
+const template_id = "template_Q8xb3sx7";//Formato de la api
+const template_id_scio = "email_notification_scio";//Email de notificaciones
+const user_id = 'user_miSxUoip92knRTffaAMa6';//Llave de mi correo personal {Se cambiara por otro despues}
 
 export default function sendEmail(e) {//Funcion para mandar email
   e.preventDefault();
-  console.log(e.target);
-  var template_params = {
+
+  const template_params = {
     "from_name": "Scio | End-to-end Software Engineering ",
     "name":e.target.user_name.value,
     "email":e.target.user_email.value, //Correo al que se mandara la info
-    "message_html": "Total development cost: $"+e.target.total.value //La info de interes sobre el costo
-    //"message_html": <templateEmail></templateEmail>,
+    "message_html": "$"+e.target.total.value //La info de interes sobre el costo
    }
 
-   var service_id = "default_service";//Gmail
-   var template_id = "template_Q8xb3sx7";//Formato de la api
-   var user_id = 'user_miSxUoip92knRTffaAMa6';//Llave de mi correo personal {Se cambiara por otro despues}
+   
    emailjs.send(service_id, template_id, template_params, user_id )
+   .then((result) => {
+      console.log(result.text);
+      sendEmailScio(e);
+  }, (error) => {
+      console.log(error.text);
+  });
+}
+
+function sendEmailScio(e){
+  e.preventDefault();
+
+  var template_params = {
+    "from_name": "Scio VMA Notification",
+    "name":e.target.user_name.value,
+    "email":e.target.user_email.value, //Correo al que se mandara la info
+    "email_scio":"",
+    "message_html": "$ "+e.target.total.value //La info de interes sobre el costo
+   }
+
+   emailjs.send(service_id, template_id_scio, template_params, user_id )
    .then((result) => {
       console.log(result.text);
   }, (error) => {
