0f8b28fd5471fe03a0f3d158b6b80bdf676d5567
Felipe Ayala <pipe.jefrey@gmail.com>
2020-02-26 16:44:30 -0600

    depure models.

--- src/database/models/Question.ts
+++ src/database/models/Question.ts
@@ -19,32 +19,3 @@ export class Question extends Model<Question> {
 
 
 
-// import { Answer } from "./answer";
-// //import { Model } from "sequelize/types";
-// import Sequelize from 'sequelize';
-// const sequelize = require('sequelize');
-
-// export class Question extends Sequelize.Model{}
-// Question.init({
-// content: Sequelize.TEXT
-// },{sequelize, modelName: 'question'})
-// Question.hasMany(Answer); 
-
-
-// export default (sequelize, DataTypes) => {
-//   const Question = sequelize.define('Question', {
-   
-//     content: DataTypes.TEXT
-//   }, {});
-//   Question.associate = function(models) {
-//     // associations can be defined here
-//     Question.hasMany(answer); 
-//   };
-//   console.log(Question.findall({}));
-//   return Question;
-// };
-
-
-
-
-
