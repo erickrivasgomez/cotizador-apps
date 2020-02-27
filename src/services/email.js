f66fcc7fa6629a15b056337fcfe4991137f3ef04
marioCarbajalScio <60710502+marioCarbajalScio@users.noreply.github.com>
2020-02-27 09:20:48 -0600

    Cambio correos oficiales

--- src/services/email.js
+++ src/services/email.js
@@ -1,10 +1,10 @@
 import emailjs from 'emailjs-com';
 
-const service_id = "default_service";//Gmail
-const template_id = "template_Q8xb3sx7";//Formato de la api
-const template_id_scio = "email_notification_scio";//Email de notificaciones de Rod
-const user_id = 'user_miSxUoip92knRTffaAMa6';//Llave de mi correo personal {Se cambiara por otro despues}
-const email_scio = "";
+const service_id = "smtp_server";//Gmail
+const template_id = "template_XWtRtIZn";//Formato de la api
+const template_id_scio = "email_template_notification";//Email de notificaciones de Rod
+const user_id = 'user_rwpKTbKkkCAKLMLlzZhJd';//Llave de mi correo personal {Se cambiara por otro despues}
+const email_scio = "info@sciodev.com";
 
 export default function sendEmail(e) {//Funcion para mandar email
   e.preventDefault();
@@ -19,12 +19,27 @@ export default function sendEmail(e) {//Funcion para mandar email
    emailjs.send(service_id, template_id, template_params, user_id )
    .then((result) => {
       console.log(result.text);
-      sendEmailScio(e);
+      //sendEmailScio(e);
+  }, (error) => {
+      console.log(error.text);
+  });
+
+  var template_params2 = {
+    "from_name": "Scio VMA Notification",
+    "name":e.target.user_name.value,
+    "email":e.target.user_email.value, 
+    "email_scio": email_scio,
+    "message_html": "$ "+e.target.total.value 
+   }
+
+  emailjs.send(service_id, template_id_scio, template_params2, user_id )
+   .then((result) => {
+      console.log(result.text);
   }, (error) => {
       console.log(error.text);
   });
 }
-
+/*
 function sendEmailScio(e){
   e.preventDefault();
 
@@ -42,4 +57,4 @@ function sendEmailScio(e){
   }, (error) => {
       console.log(error.text);
   });
-}
+}*/
