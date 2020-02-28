0e25ad3f3141ba5b5edf7a986e1e95f7f73a977c
Felipe Ayala <pipe.jefrey@gmail.com>
2020-02-27 18:20:46 -0600

    Cors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import { sequelize } from './database/connection'
 import { questionController } from './controllers/questionController'
+import cors from 'cors'
 
 
 const port = 1337
@@ -9,6 +10,13 @@ const port = 1337
 const app = express()
 app.use(bodyParser.json())
 app.use('/api', questionController)
+
+app.use(cors())
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('API is running OK')
